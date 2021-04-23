eb9bb9d1b12cf2570ac96f2a23038e2f0c5eb5f7
Tori <veganzombi@gmail.com>
2021-04-23 12:30:57 -0700

    users can now sign in

--- crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -17,13 +17,22 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
 
-        this.setState({
-            email: '',
-            password: ''
-        });
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({
+                email: '',
+                password: ''
+            });
+        } catch (error) {
+            console.log(error);
+        }
+        
+        
     }
 
     handleChange = (event) => {
@@ -42,6 +51,7 @@ class SignIn extends React.Component {
 
                 <form onSubmit={this.handleSubmit}>
                     <FormInput 
+                        name="email"
                         type="email" 
                         value={this.state.email} 
                         handleChange={this.handleChange}
@@ -49,6 +59,7 @@ class SignIn extends React.Component {
                         required />
 
                     <FormInput 
+                        name="password"
                         type="password" 
                         value={this.state.password} 
                         handleChange={this.handleChange}
