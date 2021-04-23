c48cb634edc841c6124986e607ff82ef19196d49
Tori <veganzombi@gmail.com>
2021-04-23 12:08:38 -0700

    implemented sign in with email and password with firebase

--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -9,7 +9,33 @@ const config = {
     storageBucket: "crwn-db-fa927.appspot.com",
     messagingSenderId: "24861547474",
     appId: "1:24861547474:web:724a6aac2c4b62e913460b"
-  };
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot =  await userRef.get();
+    
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
 
 firebase.initializeApp(config);
 
